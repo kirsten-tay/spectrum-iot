c09c2fb2d342ccb7ba0d17ff157e7880b4502c99
Kirsten tay <taykirsten11@gmail.com>
2022-07-24 22:25:05 +0000

    added a new section

---
+++ src/Latest.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+import { Container } from '../src/components/Container'
+
+
+export function Aboutus() {
+  return (
+    <section
+      id="Aboutus"
+      aria-labelledby="Aboutus-title"
+      className="bg-slate-50 py-20 sm:py-32 bg-black "
+    >
+
+
+      
+
+    </section>
+
+  )
+}
