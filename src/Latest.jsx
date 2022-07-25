a5c2d1d4f9d2a5372d2996b615b26db53dc301c9
Kirsten tay <taykirsten11@gmail.com>
2022-07-25 22:44:25 +0000

    updated the latest section

--- src/Latest.jsx
+++ src/Latest.jsx
@@ -10,10 +10,20 @@ export function Aboutus() {
       aria-labelledby="Aboutus-title"
       className="bg-slate-50 py-20 sm:py-32 bg-black "
     >
+       <Container>
+        <div className="mx-auto max-w-2xl md:text-center">
+          <h2
+            id="testimonials-title"
+            className=" flex justify-center font-display text-4xl  tracking-tight text-white sm:text-4xl"
+          >
+          When you know us,You love us
+          </h2>
 
+        </div>
 
-      
+      </Container>  
 
+    
     </section>
 
   )
